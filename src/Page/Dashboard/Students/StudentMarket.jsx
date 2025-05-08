402e87c38c7d4e3dcedd8476346a9a4c119ec010
NP-SON-250 <hakizimanaalexis123@gmail.com>
2025-05-08 22:31:25 +0200

    Change paginations

--- src/Page/Dashboard/Students/StudentMarket.jsx
+++ src/Page/Dashboard/Students/StudentMarket.jsx
@@ -202,7 +202,7 @@ const StudentMarket = () => {
 
       {/* Pagination */}
       {totalPages > 1 && (
-        <div className="flex justify-around md:gap-[830px] gap-[150px] md:pb-0 pt-3 px-10">
+        <div className="flex justify-around md:gap-[830px] gap-[120px] md:pb-0 pt-3 px-10">
           <button
             className={`px-2 py-1 text-blue-900 rounded flex justify-center itemes-center gap-2 ${
               currentPage === 0 ? "opacity-50 cursor-not-allowed" : ""
